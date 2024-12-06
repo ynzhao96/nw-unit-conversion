b1bcd28b9853304324a1d1d0279e75f425331287
ynzhao96 <zhaoyn96@gmail.com>
2024-12-06 02:27:35 -0400

    add _app.tsx to apply global layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { ReactElement, ReactNode } from "react";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
+import Layout from "@/components/layout";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -10,9 +11,10 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
 
-export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-  // Use the layout defined at the page level, if available
-  const getLayout = Component.getLayout ?? ((page) => page);
-
-  return getLayout(<Component {...pageProps} />);
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  return (
+    <Layout>
+      <Component {...pageProps}></Component>
+    </Layout>
+  );
 }
